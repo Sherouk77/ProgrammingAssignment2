22e836a1929b6a803c4b3b7f798e43f55e84179f
Sherouk77 <63747404+Sherouk77@users.noreply.github.com>
2020-05-03 18:19:28 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programming ass. 2, cache matrix inversion functions 
+## lexical scoping
 
-## Write a short comment describing this function
+## Creating a matrix with get and set and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  ## GET for matrix
+  get <- function()x
+  ## SET ofr inverse
+  setInverse <- function(inverse) m <<- inverse
+  ## GET for inverse
+  getInverse <- function() m 
+  ## Retuning a list for set and get functions
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cachSolve returns cached inverse if already calculated instead of re calculating
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat,...)
+  x$setInverse(m)
+  m
 }
